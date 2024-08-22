ff738e935e4bb8f7115a56181525d86b10c104e1
Koji Kimura <>
2024-08-22 23:48:59 +0900

    added OscValue type

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ export { default as Step800 } from './step800';
 export { default as Step400 } from './step400';
 export { default as Motor } from './motor';
 export { default as StepSeries } from './stepSeries';
+export type { OscValue } from './types/types';
 import { StallThreshold_Step400, StallThreshold_Step800 } from './data/enum/stallThreshold';
 import { OverCurrentDetectionThreshold_Step400, OverCurrentDetectionThreshold_Step800 } from './data/enum/overCurrentDetectionThreshold';
 import { ACT, ActivationStatus, BrakeState, DestIpChanged, Direction, HomingStatus, MicroStepMode, MotorStatus, SwMode, SwState, ThermalStatus, UvloStatus } from './data/enums';
